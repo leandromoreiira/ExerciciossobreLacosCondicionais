94c55675fa22b177af0c49c35984c47a5e134ec2
Leo Victor <contato.leandrovmoreira@gmail.com>
2024-05-20 20:58:09 -0300

    Commit inicial

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Qual hora o jogo começou? ");
+int hora = int.Parse(Console.ReadLine());
+Console.WriteLine("Qual horario o jogo terminou? ");
+int horariofinal = int.Parse(Console.ReadLine());
+
+int duracao;
+
+if (hora < horariofinal) {
+    duracao = hora - horariofinal;
+}else
+{
+    duracao = 24 - hora + horariofinal;
+}
+
+Console.WriteLine("O Jogo durou "+ duracao+" Horas");
---
+++ Exercicio05/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Globalization;
+
+Console.WriteLine("Escreva o código do item: ");
+int codigo = int.Parse(Console.ReadLine());
+Console.WriteLine("Qua a quantidade");
+int quantidade = int.Parse(Console.ReadLine());
+
+double valor;
+if (codigo == 1) {
+   valor = quantidade * 4.00;
+    Console.WriteLine("Total: R$ " + valor.ToString("F2", CultureInfo.InvariantCulture));
+}
+else if (codigo == 2 ) {
+    valor = quantidade * 4.50;
+    Console.WriteLine("Total: R$ " + valor.ToString("F2",CultureInfo.InvariantCulture));
+} else if( codigo == 3) {
+    valor = quantidade * 5.00;
+    Console.WriteLine("Total: R$ " + valor.ToString("F2", CultureInfo.InvariantCulture));
+}
+else if(codigo == 4) {
+    valor = quantidade * 2.00;
+    Console.WriteLine("Total: R$ " + valor.ToString("F2", CultureInfo.InvariantCulture));
+}
+else if (codigo == 5) {
+    valor = quantidade * 1.50;
+    Console.WriteLine("Total: R$ " + valor.ToString("F2", CultureInfo.InvariantCulture));
+}
+else {
+    Console.WriteLine("O Código fornecido é invalido");
+}
+
